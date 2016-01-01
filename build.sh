1e9a46b40c1eb31d4347ab3f28c064fdbf194db0
Stephan Böhmer <boehmerst@users.noreply.github.com>
2016-01-01 17:55:09 +0100

    create target folders

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+mkdir -p class
+mkdir -p emulator
+mkdir -p vhdl
+mkdir -p verilog
+
 scalac -d class -cp ./chisel-snapshot/chisel_2.11-2.3-SNAPSHOT.jar src/*.scala
 scala -cp ./chisel-snapshot/chisel_2.11-2.3-SNAPSHOT.jar:./class TTA_BasicAlu.fu_add_and_eq_gt_gtu_ior_shl_shr_shru_sub_sxhw_sxqw_xor_run --configInstance TTA_BasicAlu.cfg_32b_always_1 --backend Chisel.TTABackend --targetDir ./verilog
 scala -cp ./chisel-snapshot/chisel_2.11-2.3-SNAPSHOT.jar:./class TTA_BasicLoadStore.ldh_ldhu_ldq_ldqu_ldw_sth_stq_stw_run --configInstance TTA_BasicLoadStore.cfg_32b_24b --backend Chisel.TTABackend --targetDir ./verilog
